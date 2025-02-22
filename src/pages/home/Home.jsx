47c63372cc5677ca1fdc42c6df6d7eaf02e4acba
arpitbhau <arpitnikhade20@gmail.com>
2025-02-22 21:34:55 +0530

    some bug fixes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
       if (error) throw error;
       
       toast.success('memory saved successfully!');
-      document.querySelector('.sheet').innerText = getCurrentDateTime(); // Clear content except datetime
+      document.querySelector('.sheet').innerText = "";
     } catch (error) {
       console.error('Error saving memory:', error.message);
       toast.error('Failed to save memory');
@@ -111,9 +111,7 @@ function Home() {
         role="textbox" 
         spellCheck="false" 
         contentEditable="true"
-      >
-        <p>{getCurrentDateTime()}</p>
-      </div>
+      ></div>
       <div onClick={() => storeContent()} className="submit cursor-pointer hover:scale-[1.1] transition-all p-3 absolute shadow-xl bottom-5 left-4 w-14 h-14 rounded-full bg-[#6dff3c]">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000"><path d="M11.602 13.7599L13.014 15.1719L21.4795 6.7063L22.8938 8.12051L13.014 18.0003L6.65 11.6363L8.06421 10.2221L10.189 12.3469L11.6025 13.7594L11.602 13.7599ZM11.6037 10.9322L16.5563 5.97949L17.9666 7.38977L13.014 12.3424L11.6037 10.9322ZM8.77698 16.5873L7.36396 18.0003L1 11.6363L2.41421 10.2221L3.82723 11.6352L3.82604 11.6363L8.77698 16.5873Z"></path></svg>
       </div>
